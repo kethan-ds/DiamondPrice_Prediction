6cfb1bbafce0a63b0f836fb5c94f7925bd107c0f
kethan-ds <129913046+kethan-ds@users.noreply.github.com>
2025-04-08 12:34:17 -0400

    error in the method

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def predict_datapoint():
         )
         final_new_data=data.get_data_as_dataframe()
         predict_pipeline=PredictPipeline()
-        pred=predict_pipeline.predicte(final_new_data)
+        pred=predict_pipeline.predict(final_new_data)
 
         results=round(pred[0],1)
         #results=pred
